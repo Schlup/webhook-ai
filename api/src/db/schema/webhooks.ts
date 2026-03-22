d903dad87a0dd6326f01d6cb8f625ce0d21e3d9f
Schlup <contasalex25@gmail.com>
2026-03-22 15:31:52 -0300

    feat: :sparkles: frotend finalizado

--- api/src/db/schema/webhooks.ts
+++ api/src/db/schema/webhooks.ts
@@ -1,16 +1,18 @@
-import { pgTable, text, integer, jsonb, timestamp } from "drizzle-orm/pg-core";
-import { uuidv7 } from "uuidv7"
+import { pgTable, text, integer, jsonb, timestamp } from 'drizzle-orm/pg-core'
+import { uuidv7 } from 'uuidv7'
 
-export const webhhooks = pgTable("webhooks", {
-    id: text().primaryKey().$defaultFn(() => uuidv7()),
-    method: text().notNull(),
-    path: text().notNull(),
-    ip: text().notNull(),
-    statusCode: integer().notNull().default(200),
-    contentType: text(),
-    contentLength: integer(),
-    queryParms: jsonb().$type<Record<string, string>>(),
-    headers: jsonb().$type<Record<string, string>>().notNull(),
-    body: text(),
-    createdAt: timestamp().notNull().defaultNow(),
+export const webhhooks = pgTable('webhooks', {
+  id: text()
+    .primaryKey()
+    .$defaultFn(() => uuidv7()),
+  method: text().notNull(),
+  path: text().notNull(),
+  ip: text().notNull(),
+  statusCode: integer().notNull().default(200),
+  contentType: text(),
+  contentLength: integer(),
+  queryParms: jsonb().$type<Record<string, string>>(),
+  headers: jsonb().$type<Record<string, string>>().notNull(),
+  body: text(),
+  createdAt: timestamp().notNull().defaultNow(),
 })
