6d3445a22c82d27a246adfe8fc39fc7031ee8738
Schlup <contasalex25@gmail.com>
2026-04-07 19:53:53 -0300

    fix: api connection with supabase fixed

--- api/src/db/schema/webhooks.ts
+++ api/src/db/schema/webhooks.ts
@@ -2,17 +2,17 @@ import { pgTable, text, integer, jsonb, timestamp } from 'drizzle-orm/pg-core'
 import { uuidv7 } from 'uuidv7'
 
 export const webhooks = pgTable('webhooks', {
-  id: text()
+  id: text('id')
     .primaryKey()
     .$defaultFn(() => uuidv7()),
-  method: text().notNull(),
-  path: text().notNull(),
-  ip: text().notNull(),
-  statusCode: integer().notNull().default(200),
-  contentType: text(),
-  contentLength: integer(),
-  queryParams: jsonb().$type<Record<string, string>>(),
-  headers: jsonb().$type<Record<string, string>>().notNull(),
-  body: text(),
-  createdAt: timestamp().notNull().defaultNow(),
+  method: text('method').notNull(),
+  path: text('path').notNull(),
+  ip: text('ip').notNull(),
+  statusCode: integer('status_code').notNull().default(200),
+  contentType: text('content_type'),
+  contentLength: integer('content_length'),
+  queryParams: jsonb('query_params').$type<Record<string, string>>(),
+  headers: jsonb('headers').$type<Record<string, string>>().notNull(),
+  body: text('body'),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
 })
